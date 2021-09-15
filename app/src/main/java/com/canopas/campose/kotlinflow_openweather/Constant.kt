9f1174df3e538b24998878f99175f64e55f41a6f
cp-radhika-s <radhika.s@canopas.com>
2021-09-15 10:39:02 +0530

    Minor change

--- app/src/main/java/com/canopas/campose/kotlinflow_openweather/Constant.kt
+++ app/src/main/java/com/canopas/campose/kotlinflow_openweather/Constant.kt
@@ -2,7 +2,7 @@ package com.canopas.campose.kotlinflow_openweather
 
 object Constant {
     val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    val APP_ID = "44b0420118ad8ba68a320302dfede6c5"
+    val APP_ID = "API_KEY"
     val DAYS_OF_WEEK = arrayOf(
         "Sunday",
         "Monday",
